9ec7f80bd359e03e405be7b8025b469fe4aeb0ea
DESKTOP-IV3OEM3\alfud <alfud9078@naver.com>
2024-09-09 02:02:45 +0900

    Swagger 설정

---
+++ src/main/java/com/example/paymentsystem/config/SwaggerConfig.java
@@ -0,0 +1,14 @@
+package com.example.paymentsystem.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI().info(new Info().title("결제 시스템 API").description("결제 시스템 API 명세").version("0.1"));
+    }
+}
